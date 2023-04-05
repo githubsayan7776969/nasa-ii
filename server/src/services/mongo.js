9b8a87e5322eb60e2a40cabacc5c0bc568f6fb4d
githubsayan7776969 <sayan8910490@gmail.com>
2023-04-05 18:25:31 +0530

    docker-container

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Update below to match your own MongoDB connection string.
-const MONGO_URL = 'mongodb+srv://sayantan_mongo:2ycYLgeTJ7GuPeKu@cluster0.wvvgihy.mongodb.net/?retryWrites=true&w=majority';
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
